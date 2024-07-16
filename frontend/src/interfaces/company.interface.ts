d6878c3b5322e430268799c74ad63c80f7828238
AQUA651 <86642639+NoriStock1983@users.noreply.github.com>
2024-07-16 22:07:11 +0900

    create SearchConditions

---
+++ frontend/src/interfaces/company.interface.ts
@@ -0,0 +1,4 @@
+export interface Companylist{
+    companycd:string;
+    companynm_short:string;
+}
---
+++ frontend/src/interfaces/search.interface.ts
@@ -0,0 +1,5 @@
+export interface SearchCondition{
+    usercd:string;
+    companycd:string;
+    deptcd:string;
+}
